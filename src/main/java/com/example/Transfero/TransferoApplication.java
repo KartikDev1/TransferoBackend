56ea49afb44479415bee9e138c7ff8e715f0c0c6
KartikDev1 <raneeyy18@gmail.com>
2025-08-01 01:15:08 +0530

    Configure env variables and cleanup secrets

--- src/main/java/com/example/Transfero/TransferoApplication.java
+++ src/main/java/com/example/Transfero/TransferoApplication.java
@@ -1,5 +1,6 @@
 package com.example.Transfero;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TransferoApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
+		System.setProperty("APP_BASE_URL", dotenv.get("APP_BASE_URL"));
+
 		SpringApplication.run(TransferoApplication.class, args);
 	}
 
